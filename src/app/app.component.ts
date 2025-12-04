d288818023de1aa74db9781e7c7def6923032adb
Andreas Tennert <andreas@qupaya.com>
2025-12-04 09:55:43 +0100

    Your 1st Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { BookCardComponent } from './book-card/book-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [BookCardComponent],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Angularian!';
+  title = 'Angular!';
 }
