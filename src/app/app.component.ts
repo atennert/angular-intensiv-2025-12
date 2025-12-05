8fc25d80a488dcb24999b51bebd2824dc95b86d6
Andreas Tennert <andreas@qupaya.com>
2025-12-05 09:47:03 +0100

    Refactor and structure project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,10 @@
 import { Component } from '@angular/core';
-import { BookCardComponent } from './book-card/book-card.component';
-import { Book } from './book';
-import { BookFilterPipe } from './book-filter/book-filter.pipe';
+import { BookComponent } from './book/book/book.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [BookCardComponent, BookFilterPipe],
+  imports: [BookComponent],
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  readonly books: Book[] = [
-    {
-      title: 'How to win friends',
-      author: 'Dale Carnegie',
-      abstract: 'How to Win Friends and Influence ...'
-    },
-    {
-      title: 'The Willpower Instinct: How Self-Control Works ...',
-      author: 'Kelly McGonigal',
-      abstract: 'Based on Stanford University ...'
-    },
-    {
-      author: 'Simon Sinek',
-      title: 'Start with WHY',
-      abstract: "START WITH WHY shows that the leaders who've ..."
-    }
-  ];
-  bookSearchTerm = '';
-
-  protected goToBookDetails(book: Book) {
-    console.log('Navigate to book details');
-    console.table(book);
-  }
-
-  protected updateBookSearchTerm(event: Event) {
-    const inputElement = event.target as HTMLInputElement;
-    this.bookSearchTerm = inputElement.value;
-  }
-}
+export class AppComponent {}
